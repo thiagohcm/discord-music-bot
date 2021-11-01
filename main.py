23dd18db701c1ea253ad6b82f1a58fa5a5347cf1
Thiago Moura <84145643+thiagohcm@users.noreply.github.com>
2021-10-31 23:41:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import keep_alive
 import discord
 from discord.ext import commands
 import os
@@ -15,8 +14,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-keep_alive.keep_alive()
-
-token = os.environ.get("Token")
+token = os.getenv("DISCORD_TOKEN")
 
 client.run(token)
